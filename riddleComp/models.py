d2871a1c5fe4220a2f084fde6026428eed2ab281
SwaminathShiju <72644852+SwaminathShiju@users.noreply.github.com>
2021-08-23 18:27:16 +0530

    Update models.py

--- riddleComp/models.py
+++ riddleComp/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+import random,string
 
+def generate():
+    while True:
+        ret=random.choice(string.digits+string.ascii_letters, k=10)
+        if Post.objects.filer(name=ret).count()==0:
+            return ret
 
 # Create your models here.
 class Post (models.Model):
-    name = models.CharField (primary_key=True, max_length=10)
+    name = models.CharField (primary_key=True, max_length=10, default=generate)
     content = models.TextField ()
