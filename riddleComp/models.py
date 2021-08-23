97a248c9c9445ab6dcaef1b87a6f81ee2b386b31
SwaminathShiju <72644852+SwaminathShiju@users.noreply.github.com>
2021-08-23 18:47:31 +0530

    Update models.py

--- riddleComp/models.py
+++ riddleComp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import random,string
 
 def generate():
-    a=string.digits+string.ascii_letters+string.digits
+    a=string.digits+string.ascii_letters+string.digits+string.digits
     while True:
         ret=''.join(random.choices(a, k=10))
         if Post.objects.filter(name=ret).count()==0:
