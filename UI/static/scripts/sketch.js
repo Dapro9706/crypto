1f81ec3edf15ffda9f023aa0beb585fd32597d1a
SwaminathShiju <72644852+SwaminathShiju@users.noreply.github.com>
2021-08-24 08:33:07 +0530

    Update sketch.js

--- UI/static/scripts/sketch.js
+++ UI/static/scripts/sketch.js
@@ -1,3 +1,2 @@
 $('#Encode').click((event)=>{$.get("/back/crypto",{'input':$('#input').val(),'key':$('#key').val(),'reverse':0},function(data){$('#output').val(data.output)})})
 $('#Decode').click((event)=>{$.get("/back/crypto",{'input':$('#input').val(),'key':$('#key').val(),'reverse':1},function(data){$('#output').val(data.output)})})
-$('#Copy').click("click",function(){document.getElementById('output').select();document.execCommand('copy')})
