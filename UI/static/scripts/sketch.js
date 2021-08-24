1b15252a9d7014a47c544b0a3a1ec57b0bd29830
SwaminathShiju <72644852+SwaminathShiju@users.noreply.github.com>
2021-08-24 07:54:08 +0530

    Update sketch.js

--- UI/static/scripts/sketch.js
+++ UI/static/scripts/sketch.js
@@ -1,30 +1,3 @@
-
-$('#Encode').click((event)=>{
-  $.get(
-  "/back/crypto", {
-      'input':$('#input').val(),
-      'key':$('#key').val(),
-      'reverse':0
-  },
-  function(data) {
-      $('#output').val(data['output'])
-  });
-})
-
-
-$('#Decode').click((event)=>{
-  $.get(
-  "/back/crypto", {
-      'input':$('#input').val(),
-      'key':$('#key').val(),
-      'reverse':1
-  },
-  function(data) {
-      $('#output').val(data['output'])
-  });
-})
-
-$('#Copy').on("click", function(){
-    document.getElementById('output').select();
-    document.execCommand('copy');
-})
+$('#Encode').click((event)=>{$.get("/back/crypto",{'input':$('#input').val(),'key':$('#key').val(),'reverse':0},function(data){$('#output').val(data.output)})})
+$('#Decode').click((event)=>{$.get("/back/crypto",{'input':$('#input').val(),'key':$('#key').val(),'reverse':1},function(data){$('#output').val(data.output)})})
+$('#Copy').on("click",function(){document.getElementById('output').select();document.execCommand('copy')})
